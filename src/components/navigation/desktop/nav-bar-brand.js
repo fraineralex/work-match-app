c5a3103bdb11377c5649fc42c04687c98ea5f2d8
Frainer Encarnación <fraineralex2001@gmail.com>
2023-03-17 09:54:12 -0400

    [feat] change description

--- src/components/navigation/desktop/nav-bar-brand.js
+++ src/components/navigation/desktop/nav-bar-brand.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logoImage from "../../../../public/images/logo.png"
+import logoImage from "../../../../public/images/logo.png";
 
 export const NavBarBrand = () => {
   return (
